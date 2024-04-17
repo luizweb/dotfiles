8ee014cccf3beff2a53b6c2bf71a84cb4419eccd
Luiz Simoes <luizgsantonio@gmail.com>
2024-04-16 21:50:12 -0300

    first commit

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+  { "catppuccin/nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+
+  { "shaunsingh/nord.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+  },
+
+  { "navarasu/onedark.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
